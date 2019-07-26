feba741b7afb51e49beb1d175d1fa40320c74308
Matt Godbolt <matt@godbolt.org>
2019-07-26 17:35:05 -0500

    Flush progress

--- src/main/Progressifier.cpp
+++ src/main/Progressifier.cpp
@@ -8,6 +8,7 @@ void Progressifier::update(size_t numDone) noexcept {
   auto progress = static_cast<double>(numWork_ - numDone) / numWork_ * 100;
   if (progress >= lastProgress_ + minProgress_) {
     printf("%.2f%%\n", progress);
+    fflush(stdout);
     lastProgress_ = progress;
   }
 }
